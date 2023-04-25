395b3262965a8884ef58883291c0bc2f83fd5844
jazmin <jazminisabeldestefano@gmail.com>
2023-04-25 08:17:27 -0300

    add index php to integrate html css and php

---
+++ integrado/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Integrado</title>
+</head>
+<body>
+<h1>Jazmin De Stefano</h1>
+<!-- Aquí puedes agregar el contenido que desees -->
+</body>
+</html>
+
+
+<?php
