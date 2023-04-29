b0795279321b8484639ef509a72f67e36cbdaa83
jazmin <jazminisabeldestefano@gmail.com>
2023-04-29 01:37:43 -0300

    concatenar textos in the same page render

--- textos-concatenados/textosConcatenados.php
+++ textos-concatenados/textosConcatenados.php
@@ -1,3 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Textos concatenados</title>
+    <style>
+        body {
+            background: #1f2c34;
+            width: 100vw;
+            height: 100vh;
+        }
+
+        .body-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-direction: column;
+            color: white;
+        }
+
+        form {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            gap: 10px;
+        }
+    </style>
+</head>
+<body>
+
+    <header style="width: 100%; height: 100px">
+        <button style="width: 100px; height: 50px; border-radius: 10px">
+            <a href="../index.html" style="text-decoration: none; color: black">Menu principal</a>
+        </button>
+    </header>
+
+    <main class="body-container">
+
+            <h1 class="title">Textos concatenados</h1>
+            <form method="post" action="textosConcatenados.php">
+                <label for="primerTexto">Primer texto:</label>
+                <input type="text" name="primerTexto" id="primerTexto">
+                <label for="segundoTexto">Segundo texto:</label>
+                <input type="text" name="segundoTexto" id="segundoTexto">
+                <input type="submit" name="submitTexto" value="Enviar">
+            </form>
+
+    </main>
+
 <?php
 
 $primerTexto = $_POST["primerTexto"];
@@ -5,9 +54,13 @@ $segundoTexto = $_POST["segundoTexto"];
 
 function concatenar($texto1, $texto2) {
     $resultado = $texto1 . " " . $texto2;
-    return "<p style='font-size: larger'>Texto concatenado es: </p>" . "<p style='font-size: larger'>$resultado</p>";
+    return "<p style='font-size: 18px; color: white'>Texto concatenado es: </p>" . "<p style='font-size: 18px; color: white'>$resultado</p>";
 }
 
 if(isset($_POST["submitTexto"])) {
     echo concatenar($primerTexto, $segundoTexto);
-}
+} ?>
+
+</body>
+</html>
+
