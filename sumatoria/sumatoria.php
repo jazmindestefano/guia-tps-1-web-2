26443a74e40038fe994bb3023ebef1b8a23c60d1
jazmin <jazminisabeldestefano@gmail.com>
2023-04-29 01:42:26 -0300

    render in same page for sumatoria

--- sumatoria/sumatoria.php
+++ sumatoria/sumatoria.php
@@ -1,3 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sumatoria</title>
+    <style>
+        body {
+            background: #1f2c34;
+            width: 100vw;
+            height: 100vh;
+        }
+
+        .body-container {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            flex-direction: column;
+            color: white;
+        }
+
+
+        form {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            gap: 10px;
+        }
+    </style>
+</head>
+<body>
+
+    <header style="width: 100%; height: 100px">
+        <button style="width: 100px; height: 50px; border-radius: 10px">
+            <a href="../index.html" style="text-decoration: none; color: black">Menu principal</a>
+        </button>
+    </header>
+
+    <main class="body-container">
+        <h1 class="title">Sumatoria</h1>
+        <form action="sumatoria.php" method="post">
+            <label for="numeros">Ingrese una lista de numeros separados por comas:</label>
+            <input type="text" name="numeros" id="numeros" required>
+            <br><br>
+            <input type="submit" value="Calcular suma">
+        </form>
+    </main>
+</body>
+
+
 <?php
 
 // Función sumatoria_a
@@ -39,3 +88,5 @@ if (isset($_POST['numeros'])) {
     echo "<p>La suma utilizando control for each es: " . sumatoria_b($numeros) . "</p>";
     echo "<p>La suma utilizando control while es: " . sumatoria_c($numeros) . "</p>";
 }
+ ?>
+</html>
